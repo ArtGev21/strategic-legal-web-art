4201f8bf177ce6032ceb9e42ddedff4c507851ea
ArtGev21 <arturgevorgyan2011@gmail.com>
2025-05-27 14:57:01 -0700

    Updated build-manifest.json

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("4fa2a5dbd5bde075")
+/******/ 		__webpack_require__.h = () => ("c5189477cf175256")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "44aa94a4f1b03f77"; }
+/******/ 		__webpack_require__.h = function() { return "3f5f0ed8b404b2c1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
